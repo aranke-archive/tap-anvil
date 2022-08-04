64275751b1221974850448a91c8ebf14c24625a0
Kshitij Aranke <kshitij@aranke.org>
2022-08-04 00:19:54 -0700

    refactor: remove tox

--- tap_anvil/tap.py
+++ tap_anvil/tap.py
@@ -5,11 +5,8 @@ from typing import List
 from singer_sdk import Stream, Tap
 from singer_sdk import typing as th  # JSON schema typing helpers
 
-# TODO: Import your custom stream types here:
 from tap_anvil.streams import GroupsStream, UsersStream
 
-# TODO: Compile a list of custom stream types here
-#       OR rewrite discover_streams() below with your custom logic.
 STREAM_TYPES = [
     UsersStream,
     GroupsStream,
