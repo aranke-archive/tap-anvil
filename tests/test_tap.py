2f0b7793abc12ae9f5f0d5f8b774aa8c13c7f717
Kshitij Aranke <kshitij@aranke.org>
2022-08-15 10:51:36 -0700

    feat: setup v0.1 (#15)

--- tests/test_tap.py
+++ tests/test_tap.py
@@ -1,5 +1,5 @@
 """Tests standard tap features using the built-in SDK tests library."""
-
+from dotenv import load_dotenv
 from singer_sdk.testing import get_standard_tap_tests
 
 from tap_anvil.tap import TapAnvil
@@ -8,6 +8,7 @@ from tap_anvil.tap import TapAnvil
 # Run standard built-in tap tests from the SDK:
 def test_standard_tap_tests():
     """Run standard tap tests from the SDK."""
+    load_dotenv()
     tests = get_standard_tap_tests(TapAnvil)
     for test in tests:
         test()
